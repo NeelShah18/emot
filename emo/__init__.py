6d5226b9cb22e1a835d9b3d8913890c0e8720a50
Neel Shah <neelknightme@gmail.com>
2017-06-19 19:33:15 +0530

    core.py add and check working

--- emo/__init__.py
+++ emo/__init__.py
@@ -11,46 +11,14 @@ emo terminal output for Python.
 
 """
 
-
-#from emoji.core import emojize
-#from emoji.core import demojize
-#from emoji.core import get_emoji_regexp
-from emo.unicode_codes import EMO_UNICODE
-from emo.unicode_codes import UNICODE_EMO
-from emo.unicode_codes import EMOTICONS
-
+from core import emoji
+from core import emoticons
+from emo_unicode import EMO_UNICODE
+from emo_unicode import UNICODE_EMO
+from emo_unicode import EMOTICONS
 
 __version__ = '0.0.1'
 __author__ = 'Neel Shah'
 __email__ = 'neelknightme@gmail.com'
 __source__ = 'https://github.com/NeelShah18/emo'
-__license__ = '''
-New BSD License
-
-Copyright (c) 2017-2018, Neel Shah
-All rights reserved.
-
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-
-* Redistributions of source code must retain the above copyright notice, this
-  list of conditions and the following disclaimer.
-
-* Redistributions in binary form must reproduce the above copyright notice,
-  this list of conditions and the following disclaimer in the documentation
-  and/or other materials provided with the distribution.
-
-* The names of its contributors may not be used to endorse or promote products
-  derived from this software without specific prior written permission.
-
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-'''
+__license__ = ''' '''
--- emo/core.py
+++ emo/core.py
@@ -1,2 +1,36 @@
 import logging
 import re
+import emo_unicode
+#import emo
+
+__all__ = ['emoji','emoticons']
+
+def emoji(string):
+    __entities = []
+    pro_string = str(string)
+    for pos,ej in enumerate(pro_string):
+        if ej in emo_unicode.UNICODE_EMO:
+            __entities.append({
+                "value": ej,
+                "str_span": [pos,pos]
+                })
+    return __entities
+
+def emoticons(string):
+    __entities = []
+    pattern = u'(' + u'|'.join(k for k in emo_unicode.EMOTICONS) + u')'
+    __entities = []
+    matches = re.finditer(r"%s"%pattern,str(string),re.IGNORECASE)
+    for et in matches:
+        __entities.append({'value': et.group().strip(),
+        'str_span': [et.start(),et.end()]
+        })
+    return __entities
+
+
+if __name__=="__main__":
+    test = "I love it, 👨 :-) 🏁:-) :-)🏁 :-) 🏁"
+    print(emoji(test))
+    print(emoticons(test))
+    print(test[27:30])
+    print(test[17])
