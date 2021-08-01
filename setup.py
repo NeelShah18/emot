a4174d875b62f87b9c34125b33af3ac5c2c05e4a
neelshah18 <Black_knight--0>
2021-08-01 19:54:15 -0400

    Modifying download URL.

--- setup.py
+++ setup.py
@@ -17,5 +17,5 @@ setup(
     packages=['emot'],
     url="https://github.com/NeelShah18/emo",
     zip_safe=True,
-    download_url="https://github.com/NeelShah18/emot/archive/v2.2.tar.gz",
+    download_url="https://github.com/NeelShah18/emot/archive/refs/tags/v3.0.tar.gz",
 )
